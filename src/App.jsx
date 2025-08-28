2151f69c7c4cbf9142f6e8d2d18396f2d5df0dc6
msmacalopez <msmacalopezs@gmail.com>
2025-08-28 16:49:39 +1000

    jokes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import Joke from "./Joke.jsx";
+import jokesData from "./jokeData.js";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Joke
         setup="I told my wife she should embrace her mistakes."
         punchline="She gave me a hug."
+        upvotes={10}
+        ispun={true}
+        // array of objects [{},{},{}]
+        comments={[
+          { author: "josh", text: "mytext", title: "title" },
+          { author: "josh", text: "mytext", title: "title" },
+          { author: "josh", text: "mytext", title: "title" },
+        ]}
       />
-      <Joke setup="Why don’t programmers like nature? Too many bugs" />
+      <Joke
+        setup="Why don’t programmers like nature? Too many bugs"
+        upvotes={10}
+        ispun={true}
+        // array of objects [{},{},{}]
+        comments={[
+          { author: "josh", text: "mytext", title: "title" },
+          { author: "josh", text: "mytext", title: "title" },
+          { author: "josh", text: "mytext", title: "title" },
+        ]}
+      />
     </>
   );
 }
--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Joke = (props) => {
+  console.log(props.upvotes + 1);
+  console.log(!props.ispun);
+  console.log(props.comments);
+
   return (
     <>
       {/* changing display as style  */}
@@ -12,6 +16,9 @@ const Joke = (props) => {
       {props.punchline && (
         <p className="punchline">Punch line: {props.punchline}</p>
       )}
+      <p className="upvotes"> UpVotes: {props.upvotes}</p>
+      <p className="ispun"> IsPun: {props.ispun}</p>
+      <p></p>
       <hr />
     </>
   );
